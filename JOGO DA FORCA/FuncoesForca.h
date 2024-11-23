e74ea457c65b0d7366128ce743a84d521fa80926
Luiz Gustavo <172228305+gustavosallesz@users.noreply.github.com>
2024-11-23 18:01:10 -0300

    Add files via upload

---
+++ JOGO DA FORCA/FuncoesForca.h
@@ -0,0 +1,20 @@
+#ifndef FUNCOES_H
+#define FUNCOES_H
+
+#include <graphics.h>
+#include <windows.h>
+
+// Função para corrigir a acentuação
+void ortografia();
+
+// Seleção de modo de jogo
+void selecModo();
+
+// Escolhe uma palavra aleatória e retorna a dica associada
+char* escolhePalavra();
+
+// Função principal do jogo
+void jogo();
+
+
+#endif
